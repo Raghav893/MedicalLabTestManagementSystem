47c110809151d517c993237bd18727d5433bb0fd
Raghav893 <raghavarora2309@gmail.com>
2026-01-16 16:05:25 +0530

    initial commit

---
+++ src/main/java/com/raghav/medicallabtestmanagementsystem/MedicalLabTestManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.raghav.medicallabtestmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalLabTestManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedicalLabTestManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/raghav/medicallabtestmanagementsystem/MedicalLabTestManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raghav.medicallabtestmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalLabTestManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
