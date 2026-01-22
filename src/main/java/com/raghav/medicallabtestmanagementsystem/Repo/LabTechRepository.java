a644abddd953977eb92e4bb461842a2316f33365
Raghav893 <raghavarora2309@gmail.com>
2026-01-22 21:59:07 +0530

    created LabTechServices

--- src/main/java/com/raghav/medicallabtestmanagementsystem/Repo/LabTechRepository.java
+++ src/main/java/com/raghav/medicallabtestmanagementsystem/Repo/LabTechRepository.java
@@ -1,7 +1,9 @@
 package com.raghav.medicallabtestmanagementsystem.Repo;
 
 import com.raghav.medicallabtestmanagementsystem.Entity.LabTechnician;
+import com.raghav.medicallabtestmanagementsystem.Entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LabTechRepository extends JpaRepository<LabTechnician, Long> {
+    LabTechnician findByUsers(Users users);
 }
---
+++ src/main/java/com/raghav/medicallabtestmanagementsystem/Service/LabTechServices.java
@@ -0,0 +1,33 @@
+package com.raghav.medicallabtestmanagementsystem.Service;
+
+import com.raghav.medicallabtestmanagementsystem.DTO.LabTechProfileUpdateDTO;
+import com.raghav.medicallabtestmanagementsystem.Entity.LabTechnician;
+import com.raghav.medicallabtestmanagementsystem.Entity.Users;
+import com.raghav.medicallabtestmanagementsystem.Repo.LabTechRepository;
+import com.raghav.medicallabtestmanagementsystem.Repo.UserRepo;
+import lombok.RequiredArgsConstructor;
+import org.apache.catalina.User;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LabTechServices {
+
+    private final LabTechRepository repo;
+    private final UserRepo userRepo;
+    public void updateProfile(String username,LabTechProfileUpdateDTO dto){
+
+        Users users = userRepo.findByUsername(username);
+        if (users == null) {
+            throw new RuntimeException("User not found ");
+        }
+        LabTechnician technician = repo.findByUsers(users);
+        if (technician == null) {
+            throw new RuntimeException(" Lab Technician not found");
+        }
+        if (dto.getLabBranch() != null) technician.setLabBranch(dto.getLabBranch());
+        if (dto.getEmployeeCode() !=null) technician.setEmployeeCode(dto.getEmployeeCode());
+
+    }
+
+}
