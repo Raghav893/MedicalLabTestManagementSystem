e3ba9c57a079c447e5ded685008057257dce533d
Raghav893 <raghavarora2309@gmail.com>
2026-01-23 21:24:29 +0530

    ADded params

--- src/main/java/com/raghav/medicallabtestmanagementsystem/Controller/TestParameterController.java
+++ src/main/java/com/raghav/medicallabtestmanagementsystem/Controller/TestParameterController.java
@@ -1,4 +1,23 @@
 package com.raghav.medicallabtestmanagementsystem.Controller;
 
+import com.raghav.medicallabtestmanagementsystem.Entity.TestParameter;
+import com.raghav.medicallabtestmanagementsystem.Service.TestParamService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
 public class TestParameterController {
+    private final TestParamService service;
+
+    @PostMapping("/admin/lab-tests/{labTestId}/parameters")
+    public ResponseEntity<TestParameter> AddTestparameter(@RequestBody TestParameter testParameter, @PathVariable Long labTestId) {
+        return new ResponseEntity<>(service.addParam(testParameter,labTestId), HttpStatus.CREATED);
+    }
+
 }
--- src/main/java/com/raghav/medicallabtestmanagementsystem/Service/TestParamService.java
+++ src/main/java/com/raghav/medicallabtestmanagementsystem/Service/TestParamService.java
@@ -1,7 +1,31 @@
 package com.raghav.medicallabtestmanagementsystem.Service;
 
+import com.raghav.medicallabtestmanagementsystem.Entity.LabTest;
+import com.raghav.medicallabtestmanagementsystem.Entity.TestParameter;
+import com.raghav.medicallabtestmanagementsystem.Repo.LabTestRepository;
+import com.raghav.medicallabtestmanagementsystem.Repo.TestParamRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@RequiredArgsConstructor
 public class TestParamService {
-}
+
+
+        private final TestParamRepository testParameterRepository;
+        private final LabTestRepository labTestRepository;
+
+
+
+        public TestParameter addParam(TestParameter testParameter, Long labTestId) {
+
+            LabTest labTest = labTestRepository.findById(labTestId)
+                    .orElseThrow(() -> new RuntimeException("LabTest not found"));
+
+            testParameter.setLabTest(labTest);
+
+            return testParameterRepository.save(testParameter);
+        }
+    }
+
