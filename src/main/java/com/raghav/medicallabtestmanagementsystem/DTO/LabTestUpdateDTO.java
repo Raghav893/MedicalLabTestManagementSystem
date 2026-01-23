2f279b2a1ace27bfaaf2fc03a43cbd1bc33cd428
Raghav893 <raghavarora2309@gmail.com>
2026-01-23 20:48:32 +0530

    created labtest updated Dto

--- src/main/java/com/raghav/medicallabtestmanagementsystem/DTO/LabTestUpdateDTO.java
+++ src/main/java/com/raghav/medicallabtestmanagementsystem/DTO/LabTestUpdateDTO.java
@@ -1,4 +1,10 @@
 package com.raghav.medicallabtestmanagementsystem.DTO;
 
+import lombok.Data;
+
+@Data
 public class LabTestUpdateDTO {
+    String description;
+    int price;
+    boolean active;
 }
