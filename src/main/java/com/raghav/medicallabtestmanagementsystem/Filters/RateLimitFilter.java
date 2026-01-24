ed1b1de947dd7cc21e9b3477f4ce978745ce7262
Raghav893 <raghavarora2309@gmail.com>
2026-01-24 19:52:55 +0530

    Starting rate limit filter

---
+++ src/main/java/com/raghav/medicallabtestmanagementsystem/Filters/RateLimitFilter.java
@@ -0,0 +1,24 @@
+package com.raghav.medicallabtestmanagementsystem.Filters;
+
+import io.github.bucket4j.distributed.proxy.ProxyManager;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@AllArgsConstructor
+public class RateLimitFilter extends OncePerRequestFilter {
+    private final ProxyManager proxyManager;
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String path = request.getRequestURI();
+        String ip = request.getRemoteAddr();
+        String method =request.getMethod();
+    }
+}
